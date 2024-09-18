47d816b69ab9cd2056f9407cd940b76dc638604f
Ansh Adarsh <148604002+AnshCoderRepo@users.noreply.github.com>
2024-09-18 05:44:59 +0000

    next step

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,17 @@
+
+const accountId =144553
+let accountEmail="abs@gmail.com"
+var accountPassword="12345"
+
+accountCity="Gopalganj" // never but a can be declared like this also
+let accountState
+//accountId=24 //not allowed
+/*
+never use var as avarible declaration :- because in issue with block scope  and functional scope..
+*/
+accountEmail="hc@hc.com"
+accountPassword="09976"
+accountCity="absa"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";  // treat al  js code as newer version
+
+// alert(3+3)  // we are using node js not browser
+
+console.log(3+3)
+console.log("Ansh")
+
+let name ="Ansh" // String =>""
+let age =3       // Number =>2 to power of 53 , bigInt for big numbers
+let isLoggedIn= false // Boolean => True or false
+let state=null        // null => standalone value /type => empty variable but not undefined
+                      // undefined => no value assigned ; 
+                      // symbol =>find uniques  for different components
+                      
+                      // Objects
+
+
+
+console.log(typeof"Ansh");
+console.log(typeof 39);
+console.log(typeof null);
+console.log(typeof undefined); //it is already a type 
+
