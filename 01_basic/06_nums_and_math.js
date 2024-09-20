1120768ccf026c923cf73ccef85c60263bb14afe
Ansh Adarsh <148604002+AnshCoderRepo@users.noreply.github.com>
2024-09-20 05:59:19 +0000

    nums

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,30 @@
+// const score=22 //  number declarartion
+// console.log(score);
+// const balance= new Number(44) //  number delcartion another methods also gets various property with this method
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// const otherNumber=797.121331
+// console.log(otherNumber.toPrecision(6));
+// const hundereds=100000000
+// console.log(hundereds.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++
+// By default libraries
+console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.33));
+console.log(Math.ceil(4.3));
+
+console.log(Math.random()); // gives values between 0-1
+
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random() * (max-min+1))+min)
